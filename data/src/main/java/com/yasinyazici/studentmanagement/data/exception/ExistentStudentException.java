2b4bcbd42197639c2a09f9de42740d475362c382
Yasin Yazici <yasin.yazici@statista.com>
2021-08-29 22:27:31 +0200

    removed unused exceptions

--- data/src/main/java/com/yasinyazici/studentmanagement/data/exception/ExistentStudentException.java
+++
@@ -1,8 +0,0 @@
-package com.yasinyazici.studentmanagement.data.exception;
-
-public class ExistentStudentException extends IllegalStateException {
-
-    public ExistentStudentException(String name) {
-        super(String.format("Student with name %s already exists.", name));
-    }
-}
